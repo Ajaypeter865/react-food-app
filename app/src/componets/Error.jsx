9880f53528b590a6c8fe0bdbb67ba842019331a6
AjayPeter865 <ajaypeter865@gmail.com>
2026-01-04 14:31:26 +0530

    Update

--- app/src/componets/Error.jsx
+++ app/src/componets/Error.jsx
@@ -4,10 +4,11 @@ const Error = () => {
     return (
         <div>
             <h1>Oops...!!!</h1>
-            <h2>Something Went Wrong</h2>
+            <h2>Something Went Wrong..</h2>
         </div>
     )
 }
+
 
 
 export default Error
