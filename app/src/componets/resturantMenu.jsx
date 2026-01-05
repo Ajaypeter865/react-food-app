000baeba6ab0115acbf8971576b7d9dbbdcc0cdd
AjayPeter865 <ajaypeter865@gmail.com>
2026-01-05 14:27:17 +0530

    Update

--- app/src/componets/resturantMenu.jsx
+++ app/src/componets/resturantMenu.jsx
@@ -1,27 +1,45 @@
-import { useEffect } from "react"
-
+import { useEffect, useState } from "react"
+import menuData from '../../src/utils/pl.json'
+import Shimmer from "./Shimmer"
 
 const ResturantMenu = () => {
+
+    const[resInfo, setResInfo] = useState(null)
 
     useEffect(() => {
         fetchMenu()
     }, [])
 
    const fetchMenu = async () => {
-    const data = await fetch('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.8718063&lng=75.3675514&restaurantId=390778&catalog_qa=undefined&query=Biryani&submitAction=ENTER')
-    console.log('Data',data);
-    
-    const text = await data.text()
-    console.log('text',text);
+    const data = menuData
+    console.log('Data',data.data);
+    setResInfo(data.data)
 
-    const json = await data.json()
-    
-    console.log('Json',json);
-        
    }
+
+   if(resInfo === null) return <Shimmer/>
+
+   const {name, cuisines, costForTwoMessage, avgRating} = resInfo?.cards[2]?.card?.card?.info || {}
+   const {carousel} = resInfo?.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card
+   console.log(carousel);
+   
+
+   console.log('Name', name);
+   
     return(
         <div className="menu">
-            <h1>Hello from Resturant Menu </h1>
+            <h1>{name}</h1>
+            <br />
+            <p>{cuisines.join(', ')}</p>
+            <br />
+            <h2>Menu</h2>
+            <br />
+            <br />
+            <ul>
+                {carousel.map(carousel =>   <li key={carousel.dish.info.id}>{carousel.dish.info.name} - {'Rs -'}{carousel.dish.info.price/100}</li> )}
+              
+                {/* <li>{carousel[0].dish.info.name}</li> */}
+            </ul>
         </div>
     )
 }
